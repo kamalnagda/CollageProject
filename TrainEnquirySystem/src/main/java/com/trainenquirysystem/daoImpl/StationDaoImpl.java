3c7bb4e5a8736be1086cbbf604e31ac2bd8f9c32
Sonalmalviya919 <sonalimalviya7471@gmail.com>
2024-09-24 12:49:56 +0530

    creating stationdaoimpl

---
+++ TrainEnquirySystem/src/main/java/com/trainenquirysystem/daoImpl/StationDaoImpl.java
@@ -0,0 +1,41 @@
+package com.trainenquirysystem.daoImpl;
+
+import java.util.List;
+
+import com.trainenquirysystem.dao.StationDao;
+import com.trainenquirysystem.pojo.Station;
+
+public class StationDaoImpl implements StationDao {
+
+	@Override
+	  public void addStation(Station station) {
+		  
+		
+	  }
+
+	@Override
+	public Station getStationByCode(String stationCode) {
+		return null;
+		
+		
+	}
+
+	@Override
+	public List<Station> getAllStations() {
+	
+		return null;
+	}
+
+	@Override
+	public void updateStation(Station station) {
+	
+		
+	}
+
+	@Override
+	public void deleteStationByCode(String stationCode) {
+	
+		
+	}
+	
+}
