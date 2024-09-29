e947ae31d39388937d18d97dc5ceb50d81ebd67d
kamalnagda <kamalnagda.muskowl@gmail.com>
2024-09-29 21:28:53 +0530

    include sql connector jar

--- TrainEnquirySystem/src/main/java/com/trainenquirysystem/connection/DbDetails.java
+++ TrainEnquirySystem/src/main/java/com/trainenquirysystem/connection/DbDetails.java
@@ -1,7 +1,7 @@
 package com.trainenquirysystem.connection;
 
 public interface DbDetails {
-	String CONSTR = "jdbc:mysql://localhost:3306/pharmacystoredb?useSSL=false";
+	String CONSTR = "jdbc:mysql://localhost:3306/trainenquirysystemdb?useSSL=false";
 	String USERNAME = "root";
 	String PASSWORD = "root";
 	String DBDRIVER = "com.mysql.cj.jdbc.Driver";
